948db918e31228436cf9a2ed6a26e126bd1dcc74
Drew Long <andorus911@gmail.com>
2018-03-26 23:31:42 +0300

    downloadable pdf

--- routes/analyze.js
+++ routes/analyze.js
@@ -1,23 +1,44 @@
 const url = require('url');
+const fs = require('fs');
+const eventEmitter = require('events');
+const PDFDocument = require('pdfkit');
 
-const render = require('../lib/render');
 const urlAnalyze = require('../lib/urlAnalyze');
 
 function analyze(request, response) {
-    render('index.html', (error, html) =>{
-        if (error) {
-            response.writeHead(500, { 'Content-Type': 'text/plain' });
-            return response.end(error.message);
-        }
 
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'text/html');
-        response.end(html);
+    const emitter = new eventEmitter.EventEmitter();
+    const urls = url.parse(request.url, true).query.urls.split(' ');
+
+    let table = [];
+
+    for (let url of urls)
+        urlAnalyze(url, analizedWords => {
+            table = table.concat({
+                url,
+                bestOfTheBest: Array.of(
+                    analizedWords[0].wordName,
+                    analizedWords[1].wordName,
+                    analizedWords[2].wordName)
+            });
+            if (url === urls[0]) emitter.emit('analyze:complete');
+        }); // TODO: add promises?
+
+    emitter.on('analyze:complete', () => {
+        createPDF(table, response);
+    });    
+}
+
+function createPDF(table, response) {
+    let doc = new PDFDocument();
+    doc.pipe(response);
+
+    table.forEach(row => {
+        doc.text(`${row.url} \| ${row.bestOfTheBest}`);
+        doc.text(`\n____________________________________________\n`);
     });
 
-    const urls = url.parse(request.url, true).query;
-    console.log(urls);
-    urlAnalyze(urls); // TODO: add promises?
+    doc.end();
 }
 
 module.exports = analyze;
